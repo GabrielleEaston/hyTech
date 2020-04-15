aea94e5d3b9357ed9be87fe681139f89cbec26c7
Cherelle Townes <cherelletownes@Cherelles-MacBook-Air.local>
2020-04-14 23:24:10 -0400

    Deployed react app to surge

---
+++ client/build/precache-manifest.52ddd9850aa143a540cc6d8d296dde1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7364dd2212ebf80293f13832e20087e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "080a84251d635dc1b70a",
+    "url": "/static/css/main.35852bbd.chunk.css"
+  },
+  {
+    "revision": "c433667286b01fd4f7a6",
+    "url": "/static/js/2.f97013cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f97013cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "080a84251d635dc1b70a",
+    "url": "/static/js/main.81fcb1a7.chunk.js"
+  },
+  {
+    "revision": "a15422c73947459aac30",
+    "url": "/static/js/runtime-main.2b4c64c5.js"
+  }
+]);
