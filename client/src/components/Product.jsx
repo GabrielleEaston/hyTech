0912a81499790e0b505200051d6e00534e67e0b5
Gabrielle <eastongabrielle@gmail.com>
2020-04-17 11:09:53 -0400

    add author

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -3,22 +3,23 @@ import './Product.css';
 import { Link } from 'react-router-dom'
 
 const Product = (props) => {
-  
 
- 
-    
-    return (
-      <>
-        
-            <Link className="product" to={`/products/${props._id}`}>
-          <div className="product-name">{props.name}</div>
-    <div>{props.createdAt.split('T')[0]}</div>
-                <img className="product-image" src={props.imgURL} alt={props.name} />
-          <div className="product-description">{props.description}</div>
-              <button>Read More</button>
-            </Link>
-        </>
-    )
+
+
+
+  return (
+    <>
+
+      <Link className="product" to={`/products/${props._id}`}>
+        <h5>{props.author}</h5>
+        <div className="product-name">{props.name}</div>
+        <div>{props.createdAt.split('T')[0]}</div>
+        <img className="product-image" src={props.imgURL} alt={props.name} />
+        <div className="product-description">{props.description}</div>
+        <button>Read More</button>
+      </Link>
+    </>
+  )
 }
 
 export default Product
