a1a825273de2782f682c1564b3a1105fe7d6939d
Gabrielle <eastongabrielle@gmail.com>
2020-06-17 21:08:08 -0400

    fix create post user props

--- client/src/components/PostCreate.jsx
+++ client/src/components/PostCreate.jsx
@@ -13,7 +13,7 @@ class PostCreate extends Component {
         imgURL: "",
         author: "",
         sub_title: "",
-        user_id: this.props.user.id
+        user_id: this.props.user.id || this.props.user._id
       },
       created: false
     };
@@ -49,81 +49,85 @@ class PostCreate extends Component {
       return <Redirect to={`/posts`} />;
     }
     return (
-      <Layout user={this.props.user} >
+      <Layout user={this.props.user}>
         <Container>
-        <h1>Create Your First Post</h1>
+          <h1>Create Your First Post</h1>
 
-        <Form className="create-form" onSubmit={this.handleSubmit}>
+          <Form className="create-form" onSubmit={this.handleSubmit}>
             <Form.Group controlId="formAuthorname">
-            <Form.Label className="author-name">Author Name</Form.Label>
+              <Form.Label className="author-name">Author Name</Form.Label>
               <Form.Control
-              type="text"
-              className="input-author"
-              placeholder="Author"
-              value={post.author}
-              name="author"
-              required
-              autoFocus
-              onChange={this.handleChange}
-            />
-            
-        </Form.Group>
-          
-            <Form.Group controlId="formTitle">
-              
-            <Form.Label className="title-name">Title</Form.Label>
-              <Form.Control
-              type="text"
-              className="input-name"
-              placeholder="Title"
-              value={post.name}
-              name="name"
-              required
-              autoFocus
-              onChange={this.handleChange}
-            />
-        </Form.Group>
-        <Form.Group controlId="formSubTitle">
-            <Form.Label className="subtitle-name">Subtitle</Form.Label>
-              <Form.Control
-              type="text"
-              className="input-name"
-              placeholder="Sub title"
-              value={post.sub_title}
-              name="sub_title"
-              required
-              autoFocus
-              onChange={this.handleChange}
-            />
-         </Form.Group>
+                type="text"
+                className="input-author"
+                placeholder="Author"
+                value={post.author}
+                name="author"
+                required
+                autoFocus
+                onChange={this.handleChange}
+              />
+            </Form.Group>
 
-         <Form.Group controlId="formBlogText">
-            <Form.Label className="blog-div">Write your blog here</Form.Label>
-            <Form.Control as="textarea" rows="6"
-              className="textarea-description"
-              placeholder="Blog post"
-              value={post.description}
-              name="description"
-              required
-              onChange={this.handleChange}
-            />
-         </Form.Group>
-         <Form.Group controlId="formBlogText">
+            <Form.Group controlId="formTitle">
+              <Form.Label className="title-name">Title</Form.Label>
+              <Form.Control
+                type="text"
+                className="input-name"
+                placeholder="Title"
+                value={post.name}
+                name="name"
+                required
+                autoFocus
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="formSubTitle">
+              <Form.Label className="subtitle-name">Subtitle</Form.Label>
+              <Form.Control
+                type="text"
+                className="input-name"
+                placeholder="Sub title"
+                value={post.sub_title}
+                name="sub_title"
+                required
+                autoFocus
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formBlogText">
+              <Form.Label className="blog-div">Write your blog here</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows="6"
+                className="textarea-description"
+                placeholder="Blog post"
+                value={post.description}
+                name="description"
+                required
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="formBlogText">
               <Form.Label className="image-div">Insert Image Link</Form.Label>
-            <Form.Control
-              className="input-image-link"
-              placeholder="Image Link"
-              value={post.imgURL}
-              name="imgURL"
-              required
-              onChange={this.handleChange}
-            />
-          </Form.Group>
-          <Button type="submit" className="submit-button" variant="outline-info">
-            Submit Post
-          </Button>
+              <Form.Control
+                className="input-image-link"
+                placeholder="Image Link"
+                value={post.imgURL}
+                name="imgURL"
+                required
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+            <Button
+              type="submit"
+              className="submit-button"
+              variant="outline-info"
+            >
+              Submit Post
+            </Button>
           </Form>
-          </Container>
+        </Container>
       </Layout>
     );
   }
